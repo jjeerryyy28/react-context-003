340b524ffaaa5deb557cf636d26230b6d9c5acd0
prajwalnikhar <prajwalnikhar1@gmail.com>
2023-09-20 23:49:56 +0530

    react-context-003

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HomePage from "./component/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+ReactDOM.render(<HomePage/>, document.getElementById('root'));
